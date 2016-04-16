2c06037dd3b362cf409f7a28f4af49ad6dc9a9c2
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-04-16 18:54:04 +0200

    Added FunctionHelper

---
+++ tests/Helpers/data/functionAnnotations.php
@@ -0,0 +1,21 @@
+<?php
+
+class FooClass
+{
+
+	/**
+	 * @param string $a
+	 * @param integer $b
+	 * @return boolean
+	 */
+	public function withAnnotations($a, $b)
+	{
+		return true;
+	}
+
+	private function withoutAnnotations()
+	{
+		
+	}
+
+}
