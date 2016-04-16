a9d3f80a7160f41b93bb398856d3e6646a0f256c
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-04-16 18:54:04 +0200

    Added FunctionHelper

---
+++ tests/Helpers/data/functionAbstractOrNot.php
@@ -0,0 +1,25 @@
+<?php
+
+class FooClass
+{
+
+	public function fooMethod()
+	{
+
+	}
+
+}
+
+abstract class FooAbstractClass
+{
+
+	abstract public function fooAbstractMethod();
+
+}
+
+interface FooInterface
+{
+
+	public function fooInterfaceMethod();
+
+}
