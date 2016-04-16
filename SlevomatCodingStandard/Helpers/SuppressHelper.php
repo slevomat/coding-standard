eaeeed0fa4466c9caab77fb96117bffaf42e33b7
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-04-16 18:54:30 +0200

    Added SuppressHelper

---
+++ SlevomatCodingStandard/Helpers/SuppressHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SlevomatCodingStandard\Helpers;
+
+class SuppressHelper
+{
+
+	const ANNOTATION = '@phpcsSuppress';
+
+	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @param integer $pointer
+	 * @param string $suppressName
+	 * @return boolean
+	 */
+	public static function isSniffSuppressed(\PHP_CodeSniffer_File $codeSnifferFile, $pointer, $suppressName)
+	{
+		return array_reduce(AnnotationHelper::getAnnotationsByName($codeSnifferFile, $pointer, self::ANNOTATION), function ($carry, $annotationContent) use ($suppressName) {
+			if ($annotationContent === $suppressName || strpos($suppressName, sprintf('%s.', $annotationContent)) === 0) {
+				$carry = true;
+			}
+			return $carry;
+		}, false);
+	}
+
+}
