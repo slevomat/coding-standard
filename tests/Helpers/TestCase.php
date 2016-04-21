6a60fd651e013b62a6f23f210b7c9309f8518d33
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-04-21 20:51:29 +0200

    Added some useful methods for helpers tests

--- tests/Helpers/TestCase.php
+++ tests/Helpers/TestCase.php
@@ -29,6 +29,82 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
 	}
 
 	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @param string $name
+	 * @return integer|null
+	 */
+	protected function findClassPointerByName(\PHP_CodeSniffer_File $codeSnifferFile, $name)
+	{
+		$tokens = $codeSnifferFile->getTokens();
+		for ($i = 0; $i < count($tokens); $i++) {
+			if ($tokens[$i]['code'] === T_STRING && $tokens[$i]['content'] === $name) {
+				$classPointer = $codeSnifferFile->findPrevious([T_CLASS, T_INTERFACE, T_TRAIT], $i - 1);
+				if ($classPointer !== false) {
+					return $classPointer;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @param string $name
+	 * @return integer|null
+	 */
+	protected function findConstantPointerByName(\PHP_CodeSniffer_File $codeSnifferFile, $name)
+	{
+		$tokens = $codeSnifferFile->getTokens();
+		for ($i = 0; $i < count($tokens); $i++) {
+			if ($tokens[$i]['code'] === T_STRING && $tokens[$i]['content'] === $name) {
+				$constantPointer = $codeSnifferFile->findPrevious(T_CONST, $i - 1);
+				if ($constantPointer !== false) {
+					return $constantPointer;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @param string $name
+	 * @return integer|null
+	 */
+	protected function findPropertyPointerByName(\PHP_CodeSniffer_File $codeSnifferFile, $name)
+	{
+		$tokens = $codeSnifferFile->getTokens();
+		for ($i = 0; $i < count($tokens); $i++) {
+			if ($tokens[$i]['code'] === T_VARIABLE && $tokens[$i]['content'] === sprintf('$%s', $name)) {
+				$propertyPointer = $codeSnifferFile->findPrevious([T_PUBLIC, T_PROTECTED, T_PRIVATE, T_STATIC], $i - 1);
+				if ($propertyPointer !== false) {
+					return $propertyPointer;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @param string $name
+	 * @return integer|null
+	 */
+	protected function findFunctionPointerByName(\PHP_CodeSniffer_File $codeSnifferFile, $name)
+	{
+		$tokens = $codeSnifferFile->getTokens();
+		for ($i = 0; $i < count($tokens); $i++) {
+			if ($tokens[$i]['code'] === T_STRING && $tokens[$i]['content'] === $name) {
+				$functionPointer = $codeSnifferFile->findPrevious(T_FUNCTION, $i - 1);
+				if ($functionPointer !== false) {
+					return $functionPointer;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
 	 * @param integer|string $code
 	 * @return string|null
 	 */
