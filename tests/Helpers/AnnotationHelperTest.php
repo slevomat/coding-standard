171da14cbb1a24d2f75ab8563d12f03ba972bcaf
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-04-21 15:57:51 +0200

    Added AnnotationHelper

---
+++ tests/Helpers/AnnotationHelperTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace SlevomatCodingStandard\Helpers;
+
+class AnnotationHelperTest extends \SlevomatCodingStandard\Helpers\TestCase
+{
+
+	/** @var \PHP_CodeSniffer_File */
+	private $testedCodeSnifferFile;
+
+	public function testClassWithAnnotation()
+	{
+		$annotations = AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findClassPointerByName($this->getTestedCodeSnifferFile(), 'WithAnnotation'), '@see');
+		$this->assertCount(1, $annotations);
+		$this->assertSame('https://www.slevomat.cz', $annotations[0]);
+	}
+
+	public function testClassWithoutAnnotation()
+	{
+		$this->assertCount(0, AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findClassPointerByName($this->getTestedCodeSnifferFile(), 'WithoutAnnotation'), '@see'));
+	}
+
+	public function testConstantWithAnnotation()
+	{
+		$annotations = AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findConstantPointerByName($this->getTestedCodeSnifferFile(), 'WITH_ANNOTATION'), '@var');
+		$this->assertCount(1, $annotations);
+		$this->assertSame('boolean', $annotations[0]);
+	}
+
+	public function testConstantWithoutAnnotation()
+	{
+		$this->assertCount(0, AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findConstantPointerByName($this->getTestedCodeSnifferFile(), 'WITHOUT_ANNOTATION'), '@var'));
+	}
+
+	public function testPropertyWithAnnotation()
+	{
+		$annotations = AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findPropertyPointerByName($this->getTestedCodeSnifferFile(), 'withAnnotation'), '@var');
+		$this->assertCount(1, $annotations);
+		$this->assertSame('integer', $annotations[0]);
+	}
+
+	public function testPropertyWithoutAnnotation()
+	{
+		$this->assertCount(0, AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findPropertyPointerByName($this->getTestedCodeSnifferFile(), 'withoutAnnotation'), '@var'));
+	}
+
+	public function testFunctionWithAnnotation()
+	{
+		$annotations = AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findFunctionPointerByName($this->getTestedCodeSnifferFile(), 'withAnnotation'), '@param');
+		$this->assertCount(2, $annotations);
+		$this->assertSame('string $a', $annotations[0]);
+		$this->assertSame('string $b', $annotations[1]);
+	}
+
+	public function testFunctionWithoutAnnotation()
+	{
+		$this->assertCount(0, AnnotationHelper::getAnnotationsByName($this->getTestedCodeSnifferFile(), $this->findFunctionPointerByName($this->getTestedCodeSnifferFile(), 'withoutAnnotation'), '@param'));
+	}
+
+	private function getTestedCodeSnifferFile()
+	{
+		if ($this->testedCodeSnifferFile === null) {
+			$this->testedCodeSnifferFile = $this->getCodeSnifferFile(
+				__DIR__ . '/data/annotation.php'
+			);
+		}
+		return $this->testedCodeSnifferFile;
+	}
+
+}
