656334a259c5258ff264bf908d10424f87e2e3b2
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-05-12 08:10:24 +0200

    Own TestCase for sniffs

--- tests/Sniffs/Namespaces/DisallowGroupUseSniffTest.php
+++ tests/Sniffs/Namespaces/DisallowGroupUseSniffTest.php
@@ -12,12 +12,10 @@ class DisallowGroupUseSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
 
 	public function testErrors()
 	{
-		$errors = $this->checkFile(__DIR__ . '/data/disallowGroupUseErrors.php')->getErrors();
+		$codeSnifferFile = $this->checkFile(__DIR__ . '/data/disallowGroupUseErrors.php');
 
-		$this->assertSame(2, count($errors));
-
-		$this->assertTrue(isset($errors[5]), sprintf('Expected error on line %s, but none occurred.', 5));
-		$this->assertTrue(isset($errors[9]), sprintf('Expected error on line %s, but none occurred.', 9));
+		$this->assertSniffError($codeSnifferFile, 5);
+		$this->assertSniffError($codeSnifferFile, 9);
 	}
 
 }
