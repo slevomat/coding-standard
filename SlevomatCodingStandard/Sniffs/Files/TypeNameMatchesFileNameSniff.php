df15856cd882ef28968faa4c8693931188bcc923
Dominik Klapuch <klapuchdominik@gmail.com>
2016-11-14 19:48:30 +0100

    Support for multiple extensions in TypeNameMatchesFileNameSniff

--- SlevomatCodingStandard/Sniffs/Files/TypeNameMatchesFileNameSniff.php
+++ SlevomatCodingStandard/Sniffs/Files/TypeNameMatchesFileNameSniff.php
@@ -13,6 +13,9 @@ class TypeNameMatchesFileNameSniff implements \PHP_CodeSniffer_Sniff
 	/** @var string[] path(string) => namespace */
 	public $rootNamespaces = [];
 
+	/** @var string[] index(integer) => extension */
+	public $extensions = ['php'];
+
 	/** @var string[] path(string) => namespace */
 	private $normalizedRootNamespaces;
 
@@ -84,7 +87,8 @@ class TypeNameMatchesFileNameSniff implements \PHP_CodeSniffer_Sniff
 		if ($this->namespaceExtractor === null) {
 			$this->namespaceExtractor = new FilepathNamespaceExtractor(
 				$this->getRootNamespaces(),
-				$this->getSkipDirs()
+				$this->getSkipDirs(),
+				$this->extensions
 			);
 		}
 
--- tests/Sniffs/Files/FilepathNamespaceExtractorTest.php
+++ tests/Sniffs/Files/FilepathNamespaceExtractorTest.php
@@ -16,46 +16,77 @@ class FilepathNamespaceExtractorTest extends \SlevomatCodingStandard\Sniffs\Test
 			[
 				$root . '/unknown',
 				null,
+				['php'],
 			],
 			[
 				$root . '/app',
 				null,
+				['php'],
 			],
 			[
 				$root . '/app/components',
 				null,
+				['php'],
 			],
 			[
 				$root . '/app/components.php',
 				'Slevomat\\components',
+				['php'],
 			],
 			[
 				$root . '/app/Foo/Foo.swift',
 				null,
+				['php'],
 			],
 			[
 				$root . '/app/Foo/Foo.php',
 				'Slevomat\\Foo\\Foo',
+				['php'],
 			],
 			[
 				$root . '/tests/UnitTestCase.php',
 				'Slevomat\\UnitTestCase',
+				['php'],
 			],
 			[
 				$root . '/app/model/Auth/DatabaseAuthenticator.php',
 				'Slevomat\\Auth\\DatabaseAuthenticator',
+				['php'],
 			],
 			[
 				$root . '/app/services/Delivery/Premise/AbstractPremiseImporter.php',
 				'Slevomat\\Delivery\\Premise\\AbstractPremiseImporter',
+				['php'],
 			],
 			[
 				$root . '/app/ui/Admin/Newsletter/CreatePresenter.php',
 				'Slevomat\\UI\\Admin\\Newsletter\\CreatePresenter',
+				['php'],
 			],
 			[
 				$root . '/tests/services/Delivery/Goods3rd/data/StatusEnum.php',
 				'Slevomat\\Delivery\\Goods3rd\\StatusEnum',
+				['php'],
+			],
+			[
+				$root . '/tests/services/Delivery/Goods3rd/data/StatusEnum.PHP',
+				'Slevomat\\Delivery\\Goods3rd\\StatusEnum',
+				['php'],
+			],
+			[
+				$root . '/tests/services/Delivery/Goods3rd/data/StatusEnum.php',
+				'Slevomat\\Delivery\\Goods3rd\\StatusEnum',
+				['PHP'],
+			],
+			[
+				$root . '/tests/services/Delivery/Goods3rd/data/StatusEnum.php',
+				'Slevomat\\Delivery\\Goods3rd\\StatusEnum',
+				['php', 'lsp'],
+			],
+			[
+				$root . '/app/Foo/FooTest.phpt',
+				'Slevomat\\Foo\\FooTest',
+				['php', 'phpt'],
 			],
 		];
 	}
@@ -64,8 +95,9 @@ class FilepathNamespaceExtractorTest extends \SlevomatCodingStandard\Sniffs\Test
 	 * @dataProvider dataGetTypeNameFromProjectPath
 	 * @param string $path
 	 * @param string|null $expectedNamespace
+	 * @param string[] $extensions
 	 */
-	public function testGetTypeNameFromProjectPath(string $path, string $expectedNamespace = null)
+	public function testGetTypeNameFromProjectPath(string $path, string $expectedNamespace = null, array $extensions)
 	{
 		$extractor = new FilepathNamespaceExtractor(
 			[
@@ -73,7 +105,8 @@ class FilepathNamespaceExtractorTest extends \SlevomatCodingStandard\Sniffs\Test
 				'app' => 'Slevomat',
 				'tests' => 'Slevomat',
 			],
-			['components', 'forms', 'model', 'models', 'services', 'stubs', 'data']
+			['components', 'forms', 'model', 'models', 'services', 'stubs', 'data'],
+			$extensions
 		);
 		$path = str_replace('/', DIRECTORY_SEPARATOR, $path);
 		$this->assertSame($expectedNamespace, $extractor->getTypeNameFromProjectPath($path));
