b96a75638ffe20aeaff14506d514473b80ed1440
Jaroslav Hanslik <kukulich@kukulich.cz>
2016-12-28 13:26:16 +0100

    Improved iterable support

--- tests/Sniffs/Typehints/data/typehints71.php
+++ tests/Sniffs/Typehints/data/typehints71.php
@@ -14,12 +14,20 @@ class Typehints71FooClass
 	}
 
 	/**
-	 * @param iterable $list
+	 * @param string[] $list
 	 * @return void
 	 */
 	public function doBar(iterable $list): void
 	{
 		return;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function returnsIterable(): iterable
+	{
+		return [];
 	}
 
 }
