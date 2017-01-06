ea41e1ba663f9eec56e93650f7f01876a85e7289
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-01-06 16:29:08 +0100

    Added DisallowEqualOperatorsSniff

---
+++ tests/Sniffs/ControlStructures/DisallowEqualOperatorsSniffTest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\ControlStructures;
+
+class DisallowEqualOperatorsSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
+{
+
+	public function testNoErrors()
+	{
+		$this->assertNoSniffErrorInFile($this->checkFile(__DIR__ . '/data/disallowEqualOperatorsNoErrors.php'));
+	}
+
+	public function testErrors()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/disallowEqualOperatorsErrors.php');
+
+		$this->assertSame(4, $report->getErrorCount());
+
+		$this->assertSniffError($report, 3, DisallowEqualOperatorsSniff::CODE_DISALLOWED_EQUAL_OPERATOR);
+		$this->assertSniffError($report, 4, DisallowEqualOperatorsSniff::CODE_DISALLOWED_EQUAL_OPERATOR);
+		$this->assertSniffError($report, 5, DisallowEqualOperatorsSniff::CODE_DISALLOWED_NOT_EQUAL_OPERATOR);
+		$this->assertSniffError($report, 6, DisallowEqualOperatorsSniff::CODE_DISALLOWED_NOT_EQUAL_OPERATOR);
+
+		$this->assertAllFixedInFile($report);
+	}
+
+}
