ad057023afc8c82ef8ce16241ab82bba2a2ff6cd
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-02-03 23:21:04 +0100

    Fixable return type hint spacing

---
+++ tests/Sniffs/TypeHints/data/fixableReturnTypeHintNoSpaceBetweenColonAndNullabilitySymbol.php
@@ -0,0 +1,28 @@
+<?php // lint >= 7.1
+
+function foo():?Foo
+{
+
+}
+
+$callback = function():?Foo
+{
+
+};
+
+interface Foo
+{
+
+	public function doFoo($param):?Foo;
+
+}
+
+class FooBar implements Foo
+{
+
+	public function doFoo($param):?Foo
+	{
+
+	}
+
+}
