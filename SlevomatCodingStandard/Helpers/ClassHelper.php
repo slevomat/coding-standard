1b6450a31b77f99b5484df6ed032ed84c6b0fe4a
Jan Machala <jan.machala@email.cz>
2017-02-05 22:51:00 +0100

    Allow configurable FQN uses for colliding classes
    This rule allows to reference by FQN classes which have same name
    and importing both would cause collision.
    
    With this rule, program can avoid "use as" imports.

--- SlevomatCodingStandard/Helpers/ClassHelper.php
+++ SlevomatCodingStandard/Helpers/ClassHelper.php
@@ -18,4 +18,26 @@ class ClassHelper
 		return $tokens[$codeSnifferFile->findNext(T_STRING, $classPointer + 1, $tokens[$classPointer]['scope_opener'])]['content'];
 	}
 
+	/**
+	 * @param \PHP_CodeSniffer_File $codeSnifferFile
+	 * @return string[]
+	 */
+	public static function getAllNames(\PHP_CodeSniffer_File $codeSnifferFile): array
+	{
+		$getAllClassPointers = function (\PHP_CodeSniffer_File $codeSnifferFile, &$previousClassPointer): \Generator {
+			$previousClassPointer = $nextClassPointer = $codeSnifferFile->findNext(TokenHelper::$typeKeywordTokenCodes, $previousClassPointer + 1);
+			if ($nextClassPointer !== null) {
+				yield $nextClassPointer;
+			}
+		};
+		$previousClassPointer = 0;
+
+		return array_map(
+			function (int $classPointer) use ($codeSnifferFile) {
+				return self::getName($codeSnifferFile, $classPointer);
+			},
+			iterator_to_array($getAllClassPointers($codeSnifferFile, $previousClassPointer))
+		);
+	}
+
 }
---
+++ tests/Sniffs/Namespaces/data/collidingClassNameDifferentNamespaces.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alpha;
+
+use Bar\Foo;
+
+class Beta {
+
+	public function barFoo(): Foo {
+		return new Foo();
+	}
+
+	public function bazFoo(): \Baz\Foo {
+		return new \Baz\Foo();
+	}
+
+}
