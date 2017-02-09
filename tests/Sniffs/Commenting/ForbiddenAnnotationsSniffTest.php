99111d9e47bd48326b7ae7ce297434964ea412a5
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-02-09 22:24:53 +0100

    Added ForbiddenAnnotationsSniff

---
+++ tests/Sniffs/Commenting/ForbiddenAnnotationsSniffTest.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\Commenting;
+
+class ForbiddenAnnotationsSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
+{
+
+	public function testNoForbiddenAnnotations()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/noForbiddenAnnotations.php', [
+			'forbiddenAnnotations' => ['@see', '@throws'],
+		]);
+		$this->assertNoSniffErrorInFile($report);
+	}
+
+	public function testForbiddenAnnotations()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/forbiddenAnnotations.php', [
+			'forbiddenAnnotations' => ['@see', '@throws', '@Route'],
+		]);
+
+		$this->assertSame(7, $report->getErrorCount());
+
+		$this->assertSniffError($report, 5, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 6, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 19, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 21, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 30, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 32, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+		$this->assertSniffError($report, 45, ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN);
+	}
+
+	public function testFixableForbiddenAnnotations()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/forbiddenAnnotations.php', [
+			'forbiddenAnnotations' => ['@see', '@throws', '@Route'],
+		], [ForbiddenAnnotationsSniff::CODE_ANNOTATION_FORBIDDEN]);
+		$this->assertAllFixedInFile($report);
+	}
+
+}
---
+++ tests/Sniffs/Commenting/data/noForbiddenAnnotations.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @author Slevomat
+ */
+class Foo
+{
+
+	/**
+	 * @param string $a
+	 */
+	public function __construct(string $a)
+	{
+	}
+
+	/**
+	 * @return int
+	 */
+	public function get(): int
+	{
+		return 0;
+	}
+
+}
