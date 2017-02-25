b5d47e0c74fd54ec03a54fe2846fe8b14ebb2df1
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-02-25 12:38:45 +0100

    Added ParameterTypeHintSpacingSniff

---
+++ tests/Sniffs/TypeHints/ParameterTypeHintSpacingSniffTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\TypeHints;
+
+class ParameterTypeHintSpacingSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
+{
+
+	public function testNoErrors()
+	{
+		$this->assertNoSniffErrorInFile($this->checkFile(__DIR__ . '/data/parameterTypeHintSpacingNoErrors.php'));
+	}
+
+	public function testErrors()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/parameterTypeHintSpacingErrors.php');
+
+		$this->assertSame(6, $report->getErrorCount());
+
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_WHITESPACE_AFTER_NULLABILITY_SYMBOL, 'There must be no whitespace between parameter type hint nullability symbol and parameter type hint of parameter $a.');
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_NO_SPACE_BETWEEN_TYPE_HINT_AND_PARAMETER, 'There must be exactly one space between parameter type hint and parameter $a.');
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_WHITESPACE_AFTER_NULLABILITY_SYMBOL, 'There must be no whitespace between parameter type hint nullability symbol and parameter type hint of parameter $b.');
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_MULTIPLE_SPACES_BETWEEN_TYPE_HINT_AND_PARAMETER, 'There must be exactly one space between parameter type hint and reference sign of parameter $b.');
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_WHITESPACE_AFTER_NULLABILITY_SYMBOL, 'There must be no whitespace between parameter type hint nullability symbol and parameter type hint of parameter $c.');
+		$this->assertSniffError($report, 3, ParameterTypeHintSpacingSniff::CODE_MULTIPLE_SPACES_BETWEEN_TYPE_HINT_AND_PARAMETER, 'There must be exactly one space between parameter type hint and varadic parameter $c.');
+	}
+
+	public function testFixableParameterTypeHintSpacingNoSpaceBetweenTypeHintAndParameter()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/fixableParameterTypeHintSpacingNoSpaceBetweenTypeHintAndParameter.php', [], [ParameterTypeHintSpacingSniff::CODE_NO_SPACE_BETWEEN_TYPE_HINT_AND_PARAMETER]);
+		$this->assertAllFixedInFile($report);
+	}
+
+	public function testFixableParameterTypeHintSpacingMultipleSpacesBetweenTypeHintAndParameter()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/fixableParameterTypeHintSpacingMultipleSpacesBetweenTypeHintAndParameter.php', [], [ParameterTypeHintSpacingSniff::CODE_MULTIPLE_SPACES_BETWEEN_TYPE_HINT_AND_PARAMETER]);
+		$this->assertAllFixedInFile($report);
+	}
+
+	public function testFixableParameterTypeHintSpacingWhitespaceAfterNullabilitySymbol()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/fixableParameterTypeHintSpacingWhitespaceAfterNullabilitySymbol.php', [], [ParameterTypeHintSpacingSniff::CODE_WHITESPACE_AFTER_NULLABILITY_SYMBOL]);
+		$this->assertAllFixedInFile($report);
+	}
+
+}
