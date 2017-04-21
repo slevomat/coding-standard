0d4fdd95885c7dbf2c8f6c1c7d86e535cc736d64
Michael Moravec <mail@majkl578.cz>
2017-04-22 01:46:45 +0200

    ReferenceUsedNamesOnlySniff: Add option to allow fully-qualified global classes

---
+++ tests/Sniffs/Namespaces/data/fullyQualifiedGlobalClassesInNamespace.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Foo;
+
+class Bar implements \Iterator
+{
+
+	public function baz(\DateTimeInterface $date)
+	{
+
+	}
+
+}
+
+new \DateTimeImmutable();
+
+echo \DateTime::ISO8601;
