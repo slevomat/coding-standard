130711caed03f109a6a515e9ec341a648b1354d1
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-04-30 11:12:00 +0200

    Added InlineDocCommentDeclarationSniff

---
+++ tests/Sniffs/Commenting/data/invalidInlineDocCommentDeclarations.fixed.php
@@ -0,0 +1,30 @@
+<?php
+
+class Foo
+{
+
+	/** @var string */
+	private $foo;
+
+	public function __construct()
+	{
+		/** @var string[] $a */
+		$a = $this->get();
+
+		/** @see https://www.slevomat.cz */
+		$b = null;
+
+		/** @var $c */
+		$c = [];
+
+		/** @var iterable|array|\Traversable $d Lorem ipsum */
+		$d = [];
+	}
+
+	public function get()
+	{
+		$a = [];
+		return $a;
+	}
+
+}
