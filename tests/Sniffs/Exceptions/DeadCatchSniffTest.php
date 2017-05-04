c57d5c83e94a278c9848b763136ee986dfebe0d0
Jaroslav Hanslik <kukulich@kukulich.cz>
2017-05-04 22:00:49 +0200

    Cleanup

--- tests/Sniffs/Exceptions/DeadCatchSniffTest.php
+++ tests/Sniffs/Exceptions/DeadCatchSniffTest.php
@@ -14,48 +14,27 @@ class DeadCatchSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
 	public function testDeadCatchesWithoutNamespace()
 	{
 		$report = $this->checkFile(__DIR__ . '/data/deadCatchesWithoutNamespace.php');
-		$this->assertNoSniffError($report, 5);
-		$this->assertNoSniffError($report, 7);
+
+		$this->assertSame(6, $report->getErrorCount());
+
 		$this->assertSniffError($report, 9, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
 		$this->assertSniffError($report, 11, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
-		$this->assertNoSniffError($report, 17);
-		$this->assertNoSniffError($report, 19);
-		$this->assertNoSniffError($report, 21);
 		$this->assertSniffError($report, 23, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
-
-		$this->assertNoSniffError($report, 29);
-		$this->assertNoSniffError($report, 31);
 		$this->assertSniffError($report, 33, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
 		$this->assertSniffError($report, 35, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
-		$this->assertNoSniffError($report, 41);
-		$this->assertNoSniffError($report, 43);
-		$this->assertNoSniffError($report, 45);
 		$this->assertSniffError($report, 47, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
 	}
 
 	public function testDeadCatchesInNamespace()
 	{
 		$report = $this->checkFile(__DIR__ . '/data/deadCatches.php');
-		$this->assertNoSniffError($report, 9);
-		$this->assertNoSniffError($report, 15);
-		$this->assertNoSniffError($report, 21);
-		$this->assertNoSniffError($report, 27);
-		$this->assertNoSniffError($report, 33);
-		$this->assertNoSniffError($report, 35);
-		$this->assertNoSniffError($report, 37);
-		$this->assertNoSniffError($report, 39);
-		$this->assertNoSniffError($report, 45);
-		$this->assertNoSniffError($report, 47);
+
+		$this->assertSame(4, $report->getErrorCount());
+
 		$this->assertSniffError($report, 49, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
 		$this->assertSniffError($report, 51, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
-		$this->assertNoSniffError($report, 57);
-		$this->assertNoSniffError($report, 59);
 		$this->assertSniffError($report, 61, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
 		$this->assertSniffError($report, 63, DeadCatchSniff::CODE_CATCH_AFTER_THROWABLE_CATCH);
-		$this->assertNoSniffError($report, 69);
-		$this->assertNoSniffError($report, 71);
-		$this->assertNoSniffError($report, 73);
-		$this->assertNoSniffError($report, 75);
 	}
 
 	public function testDeadUnionCatches()
