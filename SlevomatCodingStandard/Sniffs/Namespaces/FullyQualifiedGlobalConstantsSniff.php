2b1568e4b6c9f3fabd3ee3b6191ca0d1db00fee2
Jaroslav Hanslík <kukulich@kukulich.cz>
2017-08-25 16:09:17 +0200

    Added FullyQualifiedGlobalConstantsSniff

---
+++ SlevomatCodingStandard/Sniffs/Namespaces/FullyQualifiedGlobalConstantsSniff.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\Namespaces;
+
+use SlevomatCodingStandard\Helpers\NamespaceHelper;
+use SlevomatCodingStandard\Helpers\ReferencedNameHelper;
+use SlevomatCodingStandard\Helpers\UseStatementHelper;
+
+class FullyQualifiedGlobalConstantsSniff implements \PHP_CodeSniffer\Sniffs\Sniff
+{
+
+	const CODE_NON_FULLY_QUALIFIED = 'NonFullyQualified';
+
+	/**
+	 * @return mixed[]
+	 */
+	public function register(): array
+	{
+		return [
+			T_OPEN_TAG,
+		];
+	}
+
+	/**
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
+	 * @param \PHP_CodeSniffer\Files\File $phpcsFile
+	 * @param int $openTagPointer
+	 */
+	public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $openTagPointer)
+	{
+		$tokens = $phpcsFile->getTokens();
+
+		$referencedNames = ReferencedNameHelper::getAllReferencedNames($phpcsFile, $openTagPointer);
+		$useStatements = UseStatementHelper::getUseStatements($phpcsFile, $openTagPointer);
+
+		foreach ($referencedNames as $referencedName) {
+			$name = $referencedName->getNameAsReferencedInFile();
+			$referenceNamePointer = $referencedName->getStartPointer();
+
+			if (!$referencedName->isConstant()) {
+				continue;
+			}
+
+			if (NamespaceHelper::isFullyQualifiedName($name)) {
+				continue;
+			}
+
+			if (NamespaceHelper::hasNamespace($name)) {
+				continue;
+			}
+
+			if (array_key_exists($name, $useStatements)) {
+				continue;
+			}
+
+			$fix = $phpcsFile->addFixableError(sprintf('Constant %s should be referenced via a fully qualified name.', $tokens[$referenceNamePointer]['content']), $referenceNamePointer, self::CODE_NON_FULLY_QUALIFIED);
+			if ($fix) {
+				$phpcsFile->fixer->beginChangeset();
+				$phpcsFile->fixer->addContentBefore($referenceNamePointer, NamespaceHelper::NAMESPACE_SEPARATOR);
+				$phpcsFile->fixer->endChangeset();
+			}
+		}
+	}
+
+}
---
+++ tests/Sniffs/Namespaces/FullyQualifiedGlobalConstantsSniffTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\Namespaces;
+
+class FullyQualifiedGlobalConstantsSniffTest extends \SlevomatCodingStandard\Sniffs\TestCase
+{
+
+	public function testNoErrors()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/fullyQualifiedGlobalConstantsNoErrors.php');
+		$this->assertNoSniffErrorInFile($report);
+	}
+
+	public function testErrors()
+	{
+		$report = $this->checkFile(__DIR__ . '/data/fullyQualifiedGlobalConstantsErrors.php');
+
+		$this->assertSame(2, $report->getErrorCount());
+
+		$this->assertSniffError($report, 17, FullyQualifiedGlobalConstantsSniff::CODE_NON_FULLY_QUALIFIED, 'Constant PHP_VERSION should be referenced via a fully qualified name.');
+		$this->assertSniffError($report, 28, FullyQualifiedGlobalConstantsSniff::CODE_NON_FULLY_QUALIFIED, 'Constant PHP_OS should be referenced via a fully qualified name.');
+
+		$this->assertAllFixedInFile($report);
+	}
+
+}
