2643369be9c443a94315cf39d3eff68a1f585e79
Jaroslav Hanslík <kukulich@kukulich.cz>
2017-08-25 14:12:01 +0200

    It's not possible to use name the is already used

---
+++ tests/Sniffs/Namespaces/data/partlyFixableReferenceViaFullyQualifiedName.fixed.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Foo\Test\Bla;
+
+use Boo\Coo;
+use Boo\Doo as Eoo;
+use Foo;
+
+class Bar
+{
+
+	public function bar()
+	{
+		new \Coo();
+		new \Eoo();
+		new Foo();
+		new \Bububu\Bar();
+	}
+
+}
