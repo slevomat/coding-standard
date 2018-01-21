60eee4e0a732e30588b8b21e78ee31c581957827
Jaroslav Hanslík <kukulich@kukulich.cz>
2018-01-21 21:03:57 +0100

    Removed useless code

--- tests/Sniffs/TypeHints/data/typeHintDeclarationEnabledObjectTypeHintErrors.php
+++ tests/Sniffs/TypeHints/data/typeHintDeclarationEnabledObjectTypeHintErrors.php
@@ -18,37 +18,4 @@ abstract class FooClass
 
 	}
 
-	/**
-	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingReturnTypeHint
-	 * @return object|null
-	 */
-	public function withReturnTypeHintSuppress()
-	{
-		return null;
-	}
-
-	/**
-	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
-	 * @param object|null $a
-	 */
-	public function withParameterTypeHintSuppress($a)
-	{
-	}
-
-	/**
-	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.UselessDocComment
-	 * @param object|null $a
-	 */
-	public function withNullableParameterTypeHintAndSuppressedUselessDocComment(?object $a)
-	{
-	}
-
-	/**
-	 * @return object[]|null
-	 */
-	public function returnsNullableArrayOfObjects(): ?array
-	{
-		return [];
-	}
-
 }
