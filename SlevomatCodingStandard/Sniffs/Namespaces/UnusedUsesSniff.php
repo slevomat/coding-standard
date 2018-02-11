09b4ce5c86df9e071bc1e86111a7455c8a4650fa
Jaroslav Hanslík <kukulich@kukulich.cz>
2018-02-11 12:00:31 +0100

    UnusedUsesSniff: fixed more false positives

--- SlevomatCodingStandard/Sniffs/Namespaces/UnusedUsesSniff.php
+++ SlevomatCodingStandard/Sniffs/Namespaces/UnusedUsesSniff.php
@@ -110,7 +110,7 @@ class UnusedUsesSniff implements \PHP_CodeSniffer\Sniffs\Sniff
 							}
 
 							if (
-								!preg_match('~(?<=^|=|\{)(' . preg_quote($nameAsReferencedInFile, '~') . ')(?=::)~i', $annotation->getParameters(), $matches)
+								!preg_match('~(?<=^|[^a-z\s\\\\])(' . preg_quote($nameAsReferencedInFile, '~') . ')(?=::)~i', $annotation->getParameters(), $matches)
 								&& !preg_match('~(?<=@)(' . preg_quote($nameAsReferencedInFile, '~') . ')(?=\\\\|\()~i', $annotation->getParameters(), $matches)
 							) {
 								continue;
