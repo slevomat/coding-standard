fd6a6c5998dee8f382bc9e7fa4028699da3fd895
Jaroslav Hanslík <kukulich@kukulich.cz>
2018-02-12 22:53:11 +0100

    FullyQualifiedClassNameInAnnotationSniff: improved annotations parsing

--- SlevomatCodingStandard/Sniffs/Namespaces/FullyQualifiedClassNameInAnnotationSniff.php
+++ SlevomatCodingStandard/Sniffs/Namespaces/FullyQualifiedClassNameInAnnotationSniff.php
@@ -2,6 +2,7 @@
 
 namespace SlevomatCodingStandard\Sniffs\Namespaces;
 
+use SlevomatCodingStandard\Helpers\AnnotationHelper;
 use SlevomatCodingStandard\Helpers\TokenHelper;
 use SlevomatCodingStandard\Helpers\TypeHelper;
 use SlevomatCodingStandard\Helpers\TypeHintHelper;
@@ -17,74 +18,78 @@ class FullyQualifiedClassNameInAnnotationSniff implements \PHP_CodeSniffer\Sniff
 	public function register(): array
 	{
 		return [
-			T_DOC_COMMENT_TAG,
+			T_DOC_COMMENT_OPEN_TAG,
 		];
 	}
 
 	/**
 	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.TypeHintDeclaration.MissingParameterTypeHint
 	 * @param \PHP_CodeSniffer\Files\File $phpcsFile
-	 * @param int $annotationTagPointer
+	 * @param int $docCommentOpenPointer
 	 */
-	public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $annotationTagPointer): void
+	public function process(\PHP_CodeSniffer\Files\File $phpcsFile, $docCommentOpenPointer): void
 	{
-		$tokens = $phpcsFile->getTokens();
+		$annotations = AnnotationHelper::getAnnotations($phpcsFile, $docCommentOpenPointer);
 
-		$annotationTagName = $tokens[$annotationTagPointer]['content'];
-
-		if (!in_array($annotationTagName, ['@var', '@param', '@return', '@throws'], true)) {
-			return;
-		}
-
-		$annotationContentPointer = TokenHelper::findNextExcluding($phpcsFile, [T_DOC_COMMENT_WHITESPACE], $annotationTagPointer + 1);
-		if ($annotationContentPointer === null || $tokens[$annotationContentPointer]['code'] !== T_DOC_COMMENT_STRING) {
-			return;
-		}
-
-		$annotationContent = trim($tokens[$annotationContentPointer]['content']);
-		if ($annotationTagName === '@var' && preg_match('~^\$\\S+\\s+(.+)~', $annotationContent, $matches)) {
-			$typeHintsDefinition = $matches[1];
-		} else {
-			$typeHintsDefinition = preg_split('~\\s+~', $annotationContent)[0];
-		}
-
-		$typeHints = explode('|', $typeHintsDefinition);
-		foreach ($typeHints as $typeHint) {
-			$typeHint = preg_replace('~(\[\])+$~', '', $typeHint);
-			$lowercasedTypeHint = strtolower($typeHint);
-			if (
-				TypeHintHelper::isSimpleTypeHint($lowercasedTypeHint)
-				|| TypeHintHelper::isSimpleUnofficialTypeHints($lowercasedTypeHint)
-				|| !TypeHelper::isTypeName($typeHint)
-			) {
+		foreach ($annotations as $annotationName => $annotationByName) {
+			if (!in_array($annotationName, ['@var', '@param', '@return', '@throws'], true)) {
 				continue;
 			}
 
-			$fullyQualifiedTypeHint = TypeHintHelper::getFullyQualifiedTypeHint($phpcsFile, $annotationTagPointer, $typeHint);
-			if ($fullyQualifiedTypeHint === $typeHint) {
-				continue;
+			foreach ($annotationByName as $annotation) {
+				if ($annotation->getContent() === null) {
+					continue;
+				}
+
+				if ($annotationName === '@var' && preg_match('~^\$\\S+\\s+(.+)~', $annotation->getContent(), $matches)) {
+					$typeHintsDefinition = $matches[1];
+				} else {
+					$typeHintsDefinition = preg_split('~\\s+~', $annotation->getContent())[0];
+				}
+
+				$typeHints = explode('|', $typeHintsDefinition);
+				foreach ($typeHints as $typeHint) {
+					$typeHint = preg_replace('~(\[\])+$~', '', $typeHint);
+					$lowercasedTypeHint = strtolower($typeHint);
+					if (
+						TypeHintHelper::isSimpleTypeHint($lowercasedTypeHint)
+						|| TypeHintHelper::isSimpleUnofficialTypeHints($lowercasedTypeHint)
+						|| !TypeHelper::isTypeName($typeHint)
+					) {
+						continue;
+					}
+
+					$fullyQualifiedTypeHint = TypeHintHelper::getFullyQualifiedTypeHint($phpcsFile, $annotation->getStartPointer(), $typeHint);
+					if ($fullyQualifiedTypeHint === $typeHint) {
+						continue;
+					}
+					$fix = $phpcsFile->addFixableError(sprintf(
+						'Class name %s in %s should be referenced via a fully qualified name.',
+						$fullyQualifiedTypeHint,
+						$annotationName
+					), $annotation->getStartPointer(), self::CODE_NON_FULLY_QUALIFIED_CLASS_NAME);
+					if (!$fix) {
+						continue;
+					}
+
+					$phpcsFile->fixer->beginChangeset();
+
+					for ($i = $annotation->getStartPointer(); $i <= $annotation->getEndPointer(); $i++) {
+						$phpcsFile->fixer->replaceToken($i, '');
+					}
+
+					$fixedAnnoationContent = preg_replace_callback(
+						'~(\s|\|)' . preg_quote($typeHint, '~') . '(\s|\||\[|$)~',
+						function (array $matches) use ($fullyQualifiedTypeHint): string {
+							return $matches[1] . $fullyQualifiedTypeHint . $matches[2];
+						},
+						TokenHelper::getContent($phpcsFile, $annotation->getStartPointer(), $annotation->getEndPointer())
+					);
+					$phpcsFile->fixer->addContent($annotation->getStartPointer(), $fixedAnnoationContent);
+
+					$phpcsFile->fixer->endChangeset();
+				}
 			}
-			$fix = $phpcsFile->addFixableError(sprintf(
-				'Class name %s in %s should be referenced via a fully qualified name.',
-				$fullyQualifiedTypeHint,
-				$annotationTagName
-			), $annotationTagPointer, self::CODE_NON_FULLY_QUALIFIED_CLASS_NAME);
-			if (!$fix) {
-				continue;
-			}
-
-			$phpcsFile->fixer->beginChangeset();
-
-			$fixedAnnoationContent = preg_replace_callback(
-				'~(^|\s|\|)' . preg_quote($typeHint, '~') . '(\s|\||\[|$)~',
-				function (array $matches) use ($fullyQualifiedTypeHint): string {
-					return $matches[1] . $fullyQualifiedTypeHint . $matches[2];
-				},
-				$tokens[$annotationContentPointer]['content']
-			);
-			$phpcsFile->fixer->replaceToken($annotationContentPointer, $fixedAnnoationContent);
-
-			$phpcsFile->fixer->endChangeset();
 		}
 	}
 
