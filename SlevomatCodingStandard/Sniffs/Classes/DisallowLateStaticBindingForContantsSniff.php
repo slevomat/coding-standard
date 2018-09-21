9b41ba31b745732d465f3f87d5194118aef4f1a1
Jaroslav Hanslík <kukulich@kukulich.cz>
2018-09-22 00:04:14 +0200

    Added DisallowLateStaticBindingForContantsSniff for BC

---
+++ SlevomatCodingStandard/Sniffs/Classes/DisallowLateStaticBindingForContantsSniff.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace SlevomatCodingStandard\Sniffs\Classes;
+
+/**
+ * @deprecated
+ * @codeCoverageIgnore
+ */
+class DisallowLateStaticBindingForContantsSniff extends DisallowLateStaticBindingForConstantsSniff
+{
+
+}
