004f7b09e1a87ec445ded71f2bae64a122abf1c5
jrfnl <jrfnl@users.noreply.github.com>
2020-01-16 12:46:13 +0100

    EarlyExit: don't break the complete PHPCS run for unsupported syntaxes
    While it is your prerogative, of course, to choose not to support certain syntaxes, when these are encountered the sniff should exit silently.
    
    As things were, the sniff would exit with an _uncaught exception_ which within PHPCS was then translated to an `Internal.Exception` error which stops the PHPCS dead for the file being scanned, this includes breaking the run for all other sniffs in a project ruleset.
    
    Exceptions are intended for developers, not for end-users.
    
    The end-user of PHPCS can do nothing with an uncaught exception. Instead, this exception should be caught within the sniff and he sniff should return silently as it couldn't be determined whether something is or isn't an error for the rules the sniff is checking for.
    
    Includes adjusted unit tests + a new unit test for control structures using alternative syntax, which clearly isn't supported either and would - until now - cause an `Undefined index: scope_condition` notice, which - again - would stop the PHPCS scan dead.

--- tests/Sniffs/ControlStructures/EarlyExitSniffTest.php
+++ tests/Sniffs/ControlStructures/EarlyExitSniffTest.php
@@ -3,7 +3,6 @@
 namespace SlevomatCodingStandard\Sniffs\ControlStructures;
 
 use SlevomatCodingStandard\Sniffs\TestCase;
-use Throwable;
 
 class EarlyExitSniffTest extends TestCase
 {
@@ -41,37 +40,44 @@ class EarlyExitSniffTest extends TestCase
 
 	public function testIfWithoutCurlyBraces(): void
 	{
-		self::expectException(Throwable::class);
-		self::expectExceptionMessage('"if" without curly braces is not supported.');
-		self::checkFile(__DIR__ . '/data/earlyExitIfWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_EARLY_EXIT_NOT_USED]);
+		$report = self::checkFile(__DIR__ . '/data/earlyExitIfWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_EARLY_EXIT_NOT_USED]);
+
+		self::assertSame(0, $report->getErrorCount());
 	}
 
 	public function testElseifWithoutCurlyBraces(): void
 	{
-		self::expectException(Throwable::class);
-		self::expectExceptionMessage('"elseif" without curly braces is not supported.');
-		self::checkFile(__DIR__ . '/data/earlyExitElseifWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+		$report = self::checkFile(__DIR__ . '/data/earlyExitElseifWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+
+		self::assertSame(0, $report->getErrorCount());
 	}
 
 	public function testElseifWithSpace(): void
 	{
-		self::expectException(Throwable::class);
-		self::expectExceptionMessage('"else" without curly braces is not supported.');
-		self::checkFile(__DIR__ . '/data/earlyExitElseifWithSpace.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+		$report = self::checkFile(__DIR__ . '/data/earlyExitElseifWithSpace.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+
+		self::assertSame(0, $report->getErrorCount());
 	}
 
 	public function testElseWithoutCurlyBraces(): void
 	{
-		self::expectException(Throwable::class);
-		self::expectExceptionMessage('"else" without curly braces is not supported.');
-		self::checkFile(__DIR__ . '/data/earlyExitElseWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_USELESS_ELSE]);
+		$report = self::checkFile(__DIR__ . '/data/earlyExitElseWithoutCurlyBraces.php', [], [EarlyExitSniff::CODE_USELESS_ELSE]);
+
+		self::assertSame(0, $report->getErrorCount());
 	}
 
 	public function testInvalidElseif(): void
 	{
-		self::expectException(Throwable::class);
-		self::expectExceptionMessage('"else" without curly braces is not supported.');
-		self::checkFile(__DIR__ . '/data/earlyExitInvalidElseif.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+		$report = self::checkFile(__DIR__ . '/data/earlyExitInvalidElseif.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+
+		self::assertSame(0, $report->getErrorCount());
+	}
+
+	public function testAlternativeSyntax(): void
+	{
+		$report = self::checkFile(__DIR__ . '/data/earlyExitAlternativeSyntax.php', [], [EarlyExitSniff::CODE_USELESS_ELSEIF]);
+
+		self::assertSame(0, $report->getErrorCount());
 	}
 
 	public function testIgnoredStandaloneIfInScopeNoErrors(): void
