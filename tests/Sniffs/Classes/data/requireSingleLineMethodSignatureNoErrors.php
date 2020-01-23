67b779b9a68c0c3424de05c9a8a00cd927c70537
Jakub Chábek <jakub.chabek@cdn77.com>
2020-01-23 14:35:38 +0100

    Fix RequireSingleLineMethodSignature for semicolons

--- tests/Sniffs/Classes/data/requireSingleLineMethodSignatureNoErrors.php
+++ tests/Sniffs/Classes/data/requireSingleLineMethodSignatureNoErrors.php
@@ -45,7 +45,7 @@ interface B
 	);
 
 	public function multiLineMethodWithPrecisely121CharsOnSingleline(
-		$someHugeVariableNameJustToFillTheSpaceBlaah
+		$someHugeVariableNameJustToFillTheSpaceBlah
 	) : void;
 }
 
