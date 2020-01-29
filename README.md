41ebdcde97269e17b7a76c8588a72219bff74de5
Richard Kapička <richard@kapicka.cz>
2020-01-29 11:45:55 +0100

    RequireSingleLineMethodSignatureSniff: Add includedMethodPatterns

--- README.md
+++ README.md
@@ -390,6 +390,8 @@ Sniff provides the following settings:
 
 * `maxLineLength`: specifies max allowed line length. If signature would fit on it, it's enforced. Use 0 value to enforce for all methods, regardless of length.
 
+* `includedMethodPatterns`: allows to configure which methods are included in sniff detection. This is an array of regular expressions (PCRE) with delimiters. You should not use this with `excludedMethodPatterns`, as it will not work properly.
+
 #### SlevomatCodingStandard.Classes.SuperfluousAbstractClassNaming
 
 Reports use of superfluous prefix or suffix "Abstract" for abstract classes.
--- tests/Sniffs/Classes/RequireSingleLineMethodSignatureSniffTest.php
+++ tests/Sniffs/Classes/RequireSingleLineMethodSignatureSniffTest.php
@@ -3,9 +3,20 @@
 namespace SlevomatCodingStandard\Sniffs\Classes;
 
 use SlevomatCodingStandard\Sniffs\TestCase;
+use Throwable;
 
 final class RequireSingleLineMethodSignatureSniffTest extends TestCase
 {
+
+	public function testThrowExceptionForInvalidPattern(): void
+	{
+		$this->expectException(Throwable::class);
+
+		self::checkFile(
+			__DIR__ . '/data/requireSingleLineMethodSignatureNoErrors.php',
+			['includedMethodPatterns' => ['invalidPattern']]
+		);
+	}
 
 	public function testNoErrors(): void
 	{
@@ -29,6 +40,20 @@ final class RequireSingleLineMethodSignatureSniffTest extends TestCase
 	public function testForAllMethods(): void
 	{
 		$report = self::checkFile(__DIR__ . '/data/requireSingleLineMethodSignatureAllMethodsErrors.php', ['maxLineLength' => 0]);
+		self::assertSame(1, $report->getErrorCount());
+
+		self::assertSniffError($report, 5, RequireSingleLineMethodSignatureSniff::CODE_REQUIRED_SINGLE_LINE_SIGNATURE);
+
+		self::assertAllFixedInFile($report);
+	}
+
+	public function testIncludedMethodPatterns(): void
+	{
+		$report = self::checkFile(__DIR__ . '/data/requireSingleLineMethodSignatureIncludedMethodsErrors.php', [
+			'maxLineLength' => 0,
+			'includedMethodPatterns' => ['/__construct/'],
+		], [RequireSingleLineMethodSignatureSniff::CODE_REQUIRED_SINGLE_LINE_SIGNATURE]);
+
 		self::assertSame(1, $report->getErrorCount());
 
 		self::assertSniffError($report, 5, RequireSingleLineMethodSignatureSniff::CODE_REQUIRED_SINGLE_LINE_SIGNATURE);
