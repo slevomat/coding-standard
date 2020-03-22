f48e0b59cdbe8f6bd1fd530bbffc6b6c8320ed59
Gabriel Caruso <carusogabriel34@gmail.com>
2020-03-22 14:57:27 +0100

    `TrailingCommaInCallSniff` requires PHP 7.3

--- tests/Sniffs/Functions/TrailingCommaInCallSniffTest.php
+++ tests/Sniffs/Functions/TrailingCommaInCallSniffTest.php
@@ -9,13 +9,17 @@ class TrailingCommaInCallSniffTest extends TestCase
 
 	public function testNoErrors(): void
 	{
-		$report = self::checkFile(__DIR__ . '/data/trailingCommaInCallNoErrors.php');
+		$report = self::checkFile(__DIR__ . '/data/trailingCommaInCallNoErrors.php', [
+			'enable' => true,
+		]);
 		self::assertNoSniffErrorInFile($report);
 	}
 
 	public function testErrors(): void
 	{
-		$report = self::checkFile(__DIR__ . '/data/trailingCommaInCallErrors.php');
+		$report = self::checkFile(__DIR__ . '/data/trailingCommaInCallErrors.php', [
+			'enable' => true,
+		]);
 
 		self::assertSame(14, $report->getErrorCount());
 
@@ -35,6 +39,16 @@ class TrailingCommaInCallSniffTest extends TestCase
 		self::assertSniffError($report, 91, TrailingCommaInCallSniff::CODE_MISSING_TRAILING_COMMA);
 
 		self::assertAllFixedInFile($report);
+	}
+
+	public function testShouldNotReportIfSniffIsDisabled(): void
+	{
+		$report = self::checkFile(__DIR__ . '/data/trailingCommaInCallErrors.php', [
+			'enable' => false,
+		]);
+
+		self::assertSame(0, $report->getErrorCount());
+		self::assertNoSniffErrorInFile($report);
 	}
 
 }
