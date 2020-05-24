db1ddcc32a2b703c6dbc4291d326db7d326f9f0f
WinterSilence <info@ensostudio.ru>
2020-05-24 09:31:53 +0300

    Update DuplicateAssignmentToVariableSniff.php

--- SlevomatCodingStandard/Sniffs/Variables/DuplicateAssignmentToVariableSniff.php
+++ SlevomatCodingStandard/Sniffs/Variables/DuplicateAssignmentToVariableSniff.php
@@ -9,8 +9,8 @@ use function in_array;
 use function sprintf;
 use const T_DOUBLE_COLON;
 use const T_EQUAL;
-use const T_SEMICOLON;
 use const T_OBJECT_OPERATOR;
+use const T_SEMICOLON;
 use const T_VARIABLE;
 
 class DuplicateAssignmentToVariableSniff implements Sniff
