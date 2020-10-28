d6a45fa4cc5c493d3a41454264d16c34d79e609e
Gabriel Caruso <carusogabriel34@gmail.com>
2020-10-28 16:20:53 +0100

    NegationOperatorSpacing: fix for magic constants

--- SlevomatCodingStandard/Sniffs/Operators/NegationOperatorSpacingSniff.php
+++ SlevomatCodingStandard/Sniffs/Operators/NegationOperatorSpacingSniff.php
@@ -11,15 +11,23 @@ use function array_merge;
 use function in_array;
 use function sprintf;
 use function strlen;
+use const T_CLASS_C;
 use const T_CLOSE_PARENTHESIS;
 use const T_CLOSE_SHORT_ARRAY;
 use const T_CLOSE_SQUARE_BRACKET;
 use const T_CONSTANT_ENCAPSED_STRING;
+use const T_DIR;
 use const T_DNUMBER;
 use const T_ENCAPSED_AND_WHITESPACE;
+use const T_FILE;
+use const T_FUNC_C;
+use const T_LINE;
 use const T_LNUMBER;
+use const T_METHOD_C;
 use const T_MINUS;
+use const T_NS_C;
 use const T_NUM_STRING;
+use const T_TRAIT_C;
 use const T_VARIABLE;
 use const T_WHITESPACE;
 
@@ -54,13 +62,21 @@ class NegationOperatorSpacingSniff implements Sniff
 			TokenHelper::getOnlyNameTokenCodes(),
 			[
 				T_CONSTANT_ENCAPSED_STRING,
+				T_CLASS_C,
 				T_CLOSE_PARENTHESIS,
 				T_CLOSE_SHORT_ARRAY,
 				T_CLOSE_SQUARE_BRACKET,
+				T_DIR,
 				T_DNUMBER,
 				T_ENCAPSED_AND_WHITESPACE,
+				T_FILE,
+				T_FUNC_C,
+				T_LINE,
 				T_LNUMBER,
+				T_METHOD_C,
+				T_NS_C,
 				T_NUM_STRING,
+				T_TRAIT_C,
 				T_VARIABLE,
 			]
 		);
