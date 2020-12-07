872fb8b1401c4343ffd36bfdf78912c2e3a56ebe
Jaroslav Hanslík <kukulich@kukulich.cz>
2020-12-07 23:56:11 +0100

    RequireSingleLineMethodSignatureSniff: More tests

--- tests/Sniffs/Classes/data/requireSingleLineMethodSignatureErrors.fixed.php
+++ tests/Sniffs/Classes/data/requireSingleLineMethodSignatureErrors.fixed.php
@@ -1,8 +1,8 @@
-<?php
+<?php // lint >= 8.0
 
 class A
 {
-	public function someMethod(int $abc, string $efg) : void {
+	public function __construct(public int|float $abc, string|false $efg) {
 	}
 
 	public function someMethodWithNoReturnType(int $abc, string $efg) {
--- tests/Sniffs/Classes/data/requireSingleLineMethodSignatureErrors.php
+++ tests/Sniffs/Classes/data/requireSingleLineMethodSignatureErrors.php
@@ -1,11 +1,11 @@
-<?php
+<?php // lint >= 8.0
 
 class A
 {
-	public function someMethod(
-		int $abc,
-		string $efg
-	) : void {
+	public function __construct(
+		public int|float $abc,
+		string|false $efg
+	) {
 	}
 
 	public function someMethodWithNoReturnType(
