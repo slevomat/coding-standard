8c738799b1039443ee3a893c2ea3428607045486
50bhan <Sobhan.Sharifi.Sadeghi@gmail.com>
2021-04-30 23:09:12 +0430

    Move FunctionLengthSniff to Functions namespace

--- tests/Sniffs/Functions/FunctionLengthSniffTest.php
+++ tests/Sniffs/Functions/FunctionLengthSniffTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace SlevomatCodingStandard\Sniffs\Files;
+namespace SlevomatCodingStandard\Sniffs\Functions;
 
 use SlevomatCodingStandard\Sniffs\TestCase;
 
