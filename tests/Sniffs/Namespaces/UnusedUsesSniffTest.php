88a787457bed12a89f5f63ffac6829d32847fc32
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-09-17 12:44:21 +0200

    UnusedUsesSniff: More tests

--- tests/Sniffs/Namespaces/UnusedUsesSniffTest.php
+++ tests/Sniffs/Namespaces/UnusedUsesSniffTest.php
@@ -94,7 +94,7 @@ class UnusedUsesSniffTest extends TestCase
 			'searchAnnotations' => false,
 		]);
 
-		self::assertSame(67, $report->getErrorCount());
+		self::assertSame(68, $report->getErrorCount());
 
 		self::assertSniffError($report, 5, UnusedUsesSniff::CODE_UNUSED_USE, 'Type Assert is not used in this file.');
 		self::assertSniffError(
@@ -187,6 +187,7 @@ class UnusedUsesSniffTest extends TestCase
 		self::assertSniffError($report, 69, UnusedUsesSniff::CODE_UNUSED_USE, 'Type SomeImportFrom1 is not used in this file.');
 		self::assertSniffError($report, 70, UnusedUsesSniff::CODE_UNUSED_USE, 'Type SomeImportFrom2 is not used in this file.');
 		self::assertSniffError($report, 71, UnusedUsesSniff::CODE_UNUSED_USE, 'Type ArrayValue is not used in this file.');
+		self::assertSniffError($report, 72, UnusedUsesSniff::CODE_UNUSED_USE, 'Type TypeAliasWithConstant is not used in this file.');
 	}
 
 	public function testUsedUseInAnnotationWithEnabledSearchAnnotations(): void
