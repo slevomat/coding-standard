f762906f0a8807bcde32a7c6e79b808a1b1208ec
Oleksandr Roslov <tr@dupkiller.net>
2021-11-23 00:15:36 +0200

    Add `SlevomatCodingStandard.Classes.RequireAbstractOrFinal`

---
+++ tests/Sniffs/Classes/data/requireAbstractOrFinalErrors.fixed.php
@@ -0,0 +1,47 @@
+<?php
+
+final class C1
+{
+
+}
+
+abstract class A1
+{
+	public $prop1;
+}
+
+final class F1
+{
+
+}
+
+final class E1 extends \Exception
+{
+
+}
+
+final
+
+class F2
+{
+
+}
+
+
+final
+// Some comment
+class F3
+{
+
+}
+
+// Another comment
+final class C2
+{
+
+}
+
+/* One more comment */ final class E2 extends \Exception
+{
+
+}
