1b70ab56feab02cba4315da4bd5de0569c11198f
Jaroslav Hanslík <kukulich@kukulich.cz>
2022-04-26 14:35:59 +0200

    SlevomatCodingStandard.Functions.DisallowTrailingCommaInDeclaration: New option "onlySingleLine"

--- tests/Sniffs/Functions/DisallowTrailingCommaInDeclarationSniffTest.php
+++ tests/Sniffs/Functions/DisallowTrailingCommaInDeclarationSniffTest.php
@@ -9,13 +9,17 @@ class DisallowTrailingCommaInDeclarationSniffTest extends TestCase
 
 	public function testNoErrors(): void
 	{
-		$report = self::checkFile(__DIR__ . '/data/disallowTrailingCommaInDeclarationNoErrors.php');
+		$report = self::checkFile(__DIR__ . '/data/disallowTrailingCommaInDeclarationNoErrors.php', [
+			'onlySingleLine' => false,
+		]);
 		self::assertNoSniffErrorInFile($report);
 	}
 
 	public function testErrors(): void
 	{
-		$report = self::checkFile(__DIR__ . '/data/disallowTrailingCommaInDeclarationErrors.php');
+		$report = self::checkFile(__DIR__ . '/data/disallowTrailingCommaInDeclarationErrors.php', [
+			'onlySingleLine' => false,
+		]);
 
 		self::assertSame(4, $report->getErrorCount());
 
@@ -23,6 +27,19 @@ class DisallowTrailingCommaInDeclarationSniffTest extends TestCase
 		self::assertSniffError($report, 14, DisallowTrailingCommaInDeclarationSniff::CODE_DISALLOWED_TRAILING_COMMA);
 		self::assertSniffError($report, 24, DisallowTrailingCommaInDeclarationSniff::CODE_DISALLOWED_TRAILING_COMMA);
 		self::assertSniffError($report, 28, DisallowTrailingCommaInDeclarationSniff::CODE_DISALLOWED_TRAILING_COMMA);
+
+		self::assertAllFixedInFile($report);
+	}
+
+	public function testWithOnlySingleLineEnabledErrors(): void
+	{
+		$report = self::checkFile(__DIR__ . '/data/disallowTrailingCommaInDeclarationWithOnlySingleLineEnabledErrors.php', [
+			'onlySingleLine' => true,
+		]);
+
+		self::assertSame(1, $report->getErrorCount());
+
+		self::assertSniffError($report, 10, DisallowTrailingCommaInDeclarationSniff::CODE_DISALLOWED_TRAILING_COMMA);
 
 		self::assertAllFixedInFile($report);
 	}
---
+++ tests/Sniffs/Functions/data/disallowTrailingCommaInDeclarationWithOnlySingleLineEnabledErrors.fixed.php
@@ -0,0 +1,12 @@
+<?php // lint >= 8.0
+
+function something(
+	$a,
+	$b,
+) {
+
+}
+
+function anything($a, $b) {
+
+}
