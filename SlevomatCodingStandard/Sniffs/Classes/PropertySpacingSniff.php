75f86f9b79a32a206d69f427f16037d20ba53bf9
Jaroslav Hanslík <kukulich@kukulich.cz>
2022-10-15 18:54:45 +0200

    Typos

--- SlevomatCodingStandard/Sniffs/Classes/PropertySpacingSniff.php
+++ SlevomatCodingStandard/Sniffs/Classes/PropertySpacingSniff.php
@@ -47,7 +47,7 @@ class PropertySpacingSniff extends AbstractPropertyAndConstantSpacing
 
 		$nextPointer = TokenHelper::findNextEffective($phpcsFile, $pointer + 1);
 		if (in_array($tokens[$nextPointer]['code'], [T_VAR, T_PUBLIC, T_PROTECTED, T_PRIVATE, T_READONLY, T_STATIC], true)) {
-			// We don't want to report the some property twice
+			// We don't want to report the same property twice
 			return $nextPointer;
 		}
 
