87a410c591fbe0ababa7e9276a6972a746224e02
Jan Esser <jane@inventis.be>
2023-01-11 11:49:06 +0100

    SlevomatCodingStandard.ControlStructures.DisallowTrailingMultiLineTernaryOperator: New sniff

---
+++ tests/Sniffs/ControlStructures/data/disallowTrailingMultiLineTernaryOperatorNoErrors.php
@@ -0,0 +1,58 @@
+<?php $a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccc';
+
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccc';
+
+$a = $b ?: true;
+
+$a = $b
+	? true
+	: false;
+
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccccccccc';
+
+$array[$b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccc'] = 'b';
+
+doSomething($b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccc');
+
+$x = doSomething(
+	sprintf(
+		'String %s %s',
+		$y,
+		$z !== 1 ? 's' : ''
+	),
+	$typeHintPointer,
+	Whatever::XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+);
+
+$x = doSomething(
+	[
+		$z !== 1 ? 's' : ''
+	],
+	$typeHintPointer,
+	Whatever::XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+);
+
+$order = [
+	UseStatement::TYPE_DEFAULT => 1,
+	UseStatement::TYPE_FUNCTION => $this->psr12Compatible ? 2 : 3,
+	UseStatement::TYPE_CONSTANT => $this->psr12Compatible ? 3 : 2,
+];
+
+// Comment
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccccccccc';
+
+// phpcs:disable ABC
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' : 'ccccccccccccc';
+
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+	? 'bbbb'
+	: 'ccccccccccccc';
+
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+	// with comments
+	? 'bbbb'
+	// with comments
+	: 'ccccccccccccc';
+
+$a = $b === 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb' ? 'bbbb' :
+	'ccccccccccccc';
