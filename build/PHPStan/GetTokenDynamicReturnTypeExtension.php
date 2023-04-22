a26ca9bf84667ac4e013d5d3c43d822f8271f25c
Jaroslav Hanslík <kukulich@kukulich.cz>
2023-04-22 10:31:36 +0200

    WIP

--- build/PHPStan/GetTokenDynamicReturnTypeExtension.php
+++ build/PHPStan/GetTokenDynamicReturnTypeExtension.php
@@ -3,11 +3,13 @@
 namespace SlevomatCodingStandard\PHPStan;
 
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
+use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
 use PHPStan\Type\IntegerType;
@@ -15,6 +17,19 @@ use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\UnionType;
+use function array_merge;
+use function array_values;
+use function in_array;
+use function is_string;
+use const T_ATTRIBUTE;
+use const T_ATTRIBUTE_END;
+use const T_CLOSE_CURLY_BRACKET;
+use const T_CLOSE_SHORT_ARRAY;
+use const T_CLOSE_SQUARE_BRACKET;
+use const T_FN;
+use const T_OPEN_CURLY_BRACKET;
+use const T_OPEN_SHORT_ARRAY;
+use const T_OPEN_SQUARE_BRACKET;
 
 class GetTokenDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
@@ -47,39 +62,86 @@ class GetTokenDynamicReturnTypeExtension implements DynamicMethodReturnTypeExten
 			$integerType = new IntegerType();
 			$stringIntegerUnion = new UnionType([$stringType, $integerType]);
 
-			$baseArrayBuilder = ConstantArrayTypeBuilder::createEmpty();
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('content'), $stringType);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('code'), $stringIntegerUnion);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('type'), $stringType);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('line'), $integerType);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('column'), $integerType);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('length'), $integerType);
-			$baseArrayBuilder->setOffsetValueType(new ConstantStringType('level'), $integerType);
+			$types = [];
 
-			$arrayBuilder = ConstantArrayTypeBuilder::createEmpty();
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('content'), $stringType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('code'), $stringIntegerUnion);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('type'), $stringType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('line'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('column'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('length'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('level'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('conditions'), new ArrayType($integerType, $stringIntegerUnion));
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('parenthesis_opener'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('parenthesis_closer'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('parenthesis_owner'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('scope_condition'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('scope_opener'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('scope_closer'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('nested_parenthesis'), new ArrayType($integerType, $integerType));
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('bracket_opener'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('bracket_closer'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('comment_opener'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('comment_closer'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('attribute_opener'), $integerType);
-			$arrayBuilder->setOffsetValueType(new ConstantStringType('attribute_closer'), $integerType);
+			$baseTokenBuilder = ConstantArrayTypeBuilder::createEmpty();
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('content'), $stringType);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('code'), $stringIntegerUnion);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('type'), $stringType);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('line'), $integerType);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('column'), $integerType);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('length'), $integerType);
+			$baseTokenBuilder->setOffsetValueType(new ConstantStringType('level'), $integerType);
 
-			$this->arrayType = new ArrayType($integerType, TypeCombinator::union($baseArrayBuilder->getArray(), $arrayBuilder->getArray()));
+			$types[] = $baseTokenBuilder->getArray();
+
+			$tokenCodesWithScope = array_merge(
+				array_values(Tokens::$scopeOpeners),
+				[T_FN]
+			);
+
+			$tokenCodesWithBrackets = [
+				T_OPEN_SHORT_ARRAY,
+				T_CLOSE_SHORT_ARRAY,
+				T_OPEN_SQUARE_BRACKET,
+				T_CLOSE_SQUARE_BRACKET,
+				T_OPEN_CURLY_BRACKET,
+				T_CLOSE_CURLY_BRACKET,
+			];
+
+			$tokenCodesWithAttributes = [
+				T_ATTRIBUTE,
+				T_ATTRIBUTE_END,
+			];
+
+			foreach (array_merge(
+				$tokenCodesWithScope,
+				$tokenCodesWithBrackets,
+				array_values(Tokens::$parenthesisOpeners),
+				array_values(Tokens::$commentTokens),
+				$tokenCodesWithAttributes
+			) as $tokenCode) {
+				$tokenBuilder = clone $baseTokenBuilder;
+
+				$codeType = is_string($tokenCode) ? new ConstantStringType($tokenCode) : new ConstantIntegerType($tokenCode);
+
+				$tokenBuilder->setOffsetValueType(new ConstantStringType('code'), $codeType);
+
+				if (in_array($tokenCode, $tokenCodesWithScope, true)) {
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('scope_condition'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('scope_opener'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('scope_closer'), $integerType);
+				}
+
+				if (in_array($tokenCode, $tokenCodesWithBrackets, true)) {
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('bracket_opener'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('bracket_closer'), $integerType);
+				}
+
+				if (in_array($tokenCode, Tokens::$parenthesisOpeners, true)) {
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('parenthesis_opener'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('parenthesis_closer'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('parenthesis_owner'), $integerType);
+				}
+
+				if (in_array($tokenCode, Tokens::$commentTokens, true)) {
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('comment_opener'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('comment_closer'), $integerType);
+				}
+
+				if (in_array($tokenCode, $tokenCodesWithAttributes, true)) {
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('attribute_opener'), $integerType);
+					$tokenBuilder->setOffsetValueType(new ConstantStringType('attribute_closer'), $integerType);
+				}
+
+				// Todo
+				$tokenBuilder->setOffsetValueType(new ConstantStringType('conditions'), new ArrayType($integerType, $stringIntegerUnion));
+				$tokenBuilder->setOffsetValueType(new ConstantStringType('nested_parenthesis'), new ArrayType($integerType, $integerType));
+
+				$types[] = $tokenBuilder->getArray();
+			}
+
+			$this->arrayType = new ArrayType($integerType, TypeCombinator::union(...$types));
 		}
 
 		return $this->arrayType;
