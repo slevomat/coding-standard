9b2c6e4d2246a344f4b0f7950564242c0c4f8b2a
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2023-12-01 16:08:21 +0100

    replace squizlabs/php_codesniffer with phpcsstandards/php_codesniffer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,4 @@
 error_reporting(E_ALL);
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/squizlabs/php_codesniffer/autoload.php';
+require __DIR__ . '/../vendor/phpcsstandards/php_codesniffer/autoload.php';
